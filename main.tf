19e9cbb552a97289d72e50b28c8a0d87ed727c0e
Alona Biletska <78759346+alonabelak@users.noreply.github.com>
2023-08-03 17:40:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-  }
-}
 provider "github" {
   token  = var.github_token
   owner = "Practical-DevOps-GitHub"
