c3d565ddb2054fa25a2b8bf368d940017e575e68
Alona Biletska <78759346+alonabelak@users.noreply.github.com>
2023-08-03 20:13:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "github" {
-  token  = var.github_token
+  token = var.token
   owner = "Practical-DevOps-GitHub"
 }
 
@@ -9,21 +9,19 @@ variable "token" {
 }
 
 variable "action_token" {
-   type        = string
-   description = "GitHub Personal Access Token"
-   sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "repository_name" {
-  description = "The name of the repository."
+  description = "(Required) The name of the repository."
   type        = string
   default     = "github-terraform-task-alonabelak"
 }
-
-resource "github_repository_collaborator" "softservedata" {
+resource "github_repository_collaborator" "collaborator" {
+  username   = "softservedata"
+  permission = "admin"
   repository = var.repository_name
-  username   = "softservedata" 
-  permission = "admin" 
 }
 
 resource "github_branch" "develop" {
@@ -42,15 +40,14 @@ resource "github_branch_protection" "main" {
   repository_id = var.repository_name
   required_pull_request_reviews {
     require_code_owner_reviews      = true
-    required_approving_review_count = 1
+    required_approving_review_count = 0
   }
 }
 
 resource "github_branch_protection" "develop" {
-  pattern ="develop"
-  repository_id=var.repository_name
+  pattern = "develop"
+  repository_id = var.repository_name
   required_pull_request_reviews {
-    dismiss_stale_reviews = true
     required_approving_review_count = 2
   }
 }
@@ -81,6 +78,7 @@ resource "github_repository_file" "pull_request_template" {
   repository          = var.repository_name
   overwrite_on_create = true
   branch              = "main"
+  overwrite_on_create = true
 }
 
 resource "github_repository_file" "codeowners_main" {
@@ -93,13 +91,12 @@ resource "github_repository_file" "codeowners_main" {
   overwrite_on_create = true
 }
 
-resource "github_repository_webhook" "discord_webhook" {
-  repository    = var.repostirot_name
-  active        = true
+resource "github_repository_webhook" "discord_server" {
+  repository    = var.repository_name
   events        = ["pull_request"]
-  configuration = { 
-    content_type = "form"    
-      url        = "https://discord.com/api/webhooks/1131652598424928266/AsBM5lLUvocbERBQglFtiDfF_J97B6AmBJ8Igc14VuONL5NcITfA_6N7R9UX5VapeMWP" 
+  configuration {
+    content_type = "form"
+    url = "https://discord.com/api/webhooks/1131652598424928266/AsBM5lLUvocbERBQglFtiDfF_J97B6AmBJ8Igc14VuONL5NcITfA_6N7R9UX5VapeMWP"
   }
 }
 resource "github_actions_secret" "pat_secret" {
