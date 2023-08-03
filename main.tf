5a3c1a3ea5beeacfb472c75e4457463c4ab80700
Alona Biletska <78759346+alonabelak@users.noreply.github.com>
2023-08-03 17:38:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,61 +6,101 @@ terraform {
     }
   }
 }
+provider "github" {
+  token  = var.github_token
+  owner = "Practical-DevOps-GitHub"
+}
+
+variable "token" {
+  type      = string
+  sensitive = true
+}
+
 variable "github_token" {
    type        = string
    description = "GitHub Personal Access Token"
-}
-provider "github" {
-  token  = var.github_token
+   sensitive = true
 }
 
-resource "github_repository" {
-  type = ""
-  name            = "github-terraform-task-alonabelak"
-  description     = "My repository description" 
-  visibility      = "public" 
 
-  default_branch  = "develop"
-
-  template {
-    owner = "Practical-DevOps-GitHub"
-    repository = "github-terraform-task-alonabelak"
-    filename = ".github/pull_request_template.md"
-  }
+variable "repository_name" {
+  description = "The name of the repository."
+  type        = string
+  default     = "github-terraform-task-alonabelak"
 }
 
 resource "github_repository_collaborator" "softservedata" {
-  repository = "github-terraform-task-alonabelak"
+  repository = var.repository_name
   username   = "softservedata" 
-  permission = "push" 
+  permission = "admin" 
+}
+
+resource "github_branch" "develop" {
+  repository    = var.repository_name
+  branch        = "develop"
+  source_branch = "main"
+}
+resource "github_branch_default" "this" {
+  branch     = "develop"
+  repository = var.repository_name
+  depends_on = [github_branch.develop]
 }
 
 resource "github_branch_protection" "main" {
-  pattern =""
-  repository_id="main_branch_protection_id"
-
-ids {
-  main_branch_protection_id = github_branch_protection.main_protection.id
-  develop_branch_protection_id = github_branch_protection.develop_protection.id
-}
-
+  pattern       = "main"
+  repository_id = var.repository_name
   required_pull_request_reviews {
-    dismiss_stale_reviews = true
-    require_code_owner_reviews = true
+    require_code_owner_reviews      = true
     required_approving_review_count = 1
   }
 }
 
 resource "github_branch_protection" "develop" {
-  pattern =""
-  repository_id="develop_branch_protection_id"
-
+  pattern ="develop"
+  repository_id=var.repostiroy_name
   required_pull_request_reviews {
     dismiss_stale_reviews = true
     required_approving_review_count = 2
   }
-
   enforce_admins = true
+}
+
+resource "tls_private_key" "deploy_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "github_repository_deploy_key" "deploy_key" {
+  key        = tls_private_key.deploy_key.public_key_openssh
+  repository = var.repository_name
+  title      = "DEPLOY_KEY"
+}
+resource "github_repository_file" "pull_request_template" {
+  content             = <<EOT
+  ## Describe your changes
+
+  ## Issue ticket number and link
+
+  ## Checklist before requesting a review
+  - [ ] I have performed a self-review of my code
+  - [ ] If it is a core feature, I have added thorough tests
+  - [ ] Do we need to implement analytics?
+  - [ ] Will this be part of a product update? If yes, please write one phrase about this update
+  EOT
+  file                = ".github/pull_request_template.md"
+  repository          = var.repository_name
+  overwrite_on_create = true
+  branch              = "main"
+}
+
+resource "github_repository_file" "codeowners_main" {
+  content             = <<EOT
+  * @softservedata
+  EOT
+  file                = "CODEOWNERS"
+  repository          = var.repository_name
+  branch              = "main"
+  overwrite_on_create = true
 }
 
 resource "github_actions_secret" "terraform_secret" {
@@ -69,8 +109,7 @@ resource "github_actions_secret" "terraform_secret" {
   value      = base64encode(file("main.tf"))
 }
 resource "github_repository_webhook" "discord_webhook" {
-  repository    = "github-terraform-task-alonabelak"
-  name          = "discord"
+  repository    = var.repostirot_name
   active        = true
   events        = ["pull_request"]
   configuration = { 
@@ -81,7 +120,7 @@ resource "github_repository_webhook" "discord_webhook" {
   }
 }
 resource "github_actions_secret" "pat_secret" {
-  repository = "github-terraform-task-alonabelak"
+  repository = var.repository_name
   secret_name = "PAT"
-  plaintext_value = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQD92B17TcO3Uhmi4Aif9Gua0xmmZArnmO48sWR/bz7OwmUiBqy3xU2v2aAulVdSdaxNiVtEnyW3Rkzg8JLePp52KE65Zn7H/jzdzXPEOrweTCWy/2fmIMcYYzkjz8+MyhSuUkvK7QsM90DPutk5Kkq0VCAwbC/9USmxboidhCuAqO1pc1pQVupBbK+9gn+6gxIsWLJBgcdPV/0pgp4hkr9+Rf5CbxM959tuVBgW8Rv5w4gikDjIRajO27CcvzYVtUMj8gA64umcpPT7iNkGsvQdY/CrrPrqtlNF5RwGIr3nYIjDj850aOqoSHC1ujlTAgx5pkqdDNZxZnsJjAs6fdr4WDbyDJ5tNoxE/rUxUuQNkUim9OPDgxy8edteVL+bgcseudL5RLzYYuIi5T30zSkPXizi6ihNapkksAWXcMgv2e+6Rhwz5GaxS6oiPHiQBj3u6m+UyhIDHtOEaxLOAI49jWJd+E7x9OviJwtKNz+1CgmHPc2WPAhJ1UTCdHNA6G8=" 
+  plaintext_value = var.action_token
 }
